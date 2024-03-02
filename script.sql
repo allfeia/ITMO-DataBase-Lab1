82deb960514cf0ae45aa3d78870fa017beb8f88f
ALISA <146487164+allfeia@users.noreply.github.com>
2024-03-02 16:46:23 +0300

    script.sql

--- script.sql
+++ script.sql
@@ -10,7 +10,7 @@ description text NOT NULL);
 CREATE TABLE observation(
 id serial PRIMARY KEY,
 change_color varchar(64),
-fied_of_view text NOT NULL,
+field_of_view text NOT NULL,
 time timestamp NOT NULL,
 place_id int NOT NULL REFERENCES place(id));
 
@@ -18,4 +18,20 @@ CREATE TABLE action(
 id serial PRIMARY KEY,
 description text NOT NULL,
 observation_id int REFERENCES observation(id));
+
+-- Insert data into table planet
+INSERT INTO planet(name) values
+('Планета');
+-- Insert data into table place
+INSERT INTO place(description) values
+('Земля');
+-- Insert data into table observation
+INSERT INTO observation(change_color, field_of_view, time, place_id) values
+('оранжевая', '-', '2022-08-15 14:30:00', '1'),
+('тускло-серая', 'кажется слабенькой звёздочкой', '2022-08-29 14:38:00', '1');
+
+//TODO: заполнить таблицу action
+
+
+
 
