7a1f46cd8efc4eb2b2a82d5a2018e8f1545af511
ALISA <146487164+allfeia@users.noreply.github.com>
2024-03-02 15:22:16 +0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,21 +1,21 @@
 -- Creation Data Base
 CREATE TABLE planet(
-id int PRIMARY KEY,
+id serial PRIMARY KEY,
 name varchar(64));
 
 CREATE TABLE place(
-id int PRIMARY KEY,
+id serial PRIMARY KEY,
 description text NOT NULL);
 
 CREATE TABLE observation(
-id int PRIMARY KEY,
+id serial PRIMARY KEY,
 change_color varchar(64),
 fied_of_view text NOT NULL,
 time timestamp NOT NULL,
 place_id int NOT NULL REFERENCES place(id));
 
 CREATE TABLE action(
-id int PRIMARY KEY,
+id serial PRIMARY KEY,
 description text NOT NULL,
 observation_id int REFERENCES observation(id));
 
